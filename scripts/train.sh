4d0a8b9025189d0462beafd4ef09e01bc47e436e
zigzagcai <zheng.cai@intel.com>
2023-04-07 21:01:47 +0000

    add exp

--- scripts/train.sh
+++ scripts/train.sh
@@ -13,25 +13,11 @@
 #     --micro_batch_size=8
 
 
-# # gpt-j
-# python finetune.py \
-#     --base_model '/home/vmagent/app/data/gpt-j-6b-1' \
-#     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
-#     --output_dir '/home/vmagent/app/data/zy_models/lora-gptj' \
-#     --batch_size 256 \
-#     --num_epochs=1 \
-#     --cutoff_len=512 \
-#     --group_by_length \
-#     --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' \
-#     --lora_r=16 \
-#     --micro_batch_size=8
-
-
-# opt
+# gpt-j
 python finetune.py \
-    --base_model '/home/vmagent/app/data/opt-6.7b' \
+    --base_model '/home/vmagent/app/data/gpt-j-6b' \
     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
-    --output_dir '/home/vmagent/app/data/zy_models/lora-opt' \
+    --output_dir '/home/vmagent/app/data/zy_models/lora-gptj' \
     --batch_size 256 \
     --num_epochs=1 \
     --cutoff_len=512 \
@@ -39,3 +25,17 @@ python finetune.py \
     --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' \
     --lora_r=16 \
     --micro_batch_size=8
+
+
+# # opt
+# python finetune.py \
+#     --base_model '/home/vmagent/app/data/opt-6.7b' \
+#     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
+#     --output_dir '/home/vmagent/app/data/zy_models/lora-opt' \
+#     --batch_size 256 \
+#     --num_epochs=1 \
+#     --cutoff_len=512 \
+#     --group_by_length \
+#     --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' \
+#     --lora_r=16 \
+#     --micro_batch_size=8
