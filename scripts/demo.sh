c63edc274900681654a3bfa0e59a7e23274fc209
zhouyu5 <yu.zhou@intel.com>
2023-04-10 19:59:57 +0000

    exp

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,6 +1,7 @@
+# for int8 training, please set this flag
+# --load_8bit \    
+
 python app.py \
-    --load_8bit \
     --share_gradio True \
-    --base_model '/home/vmagent/app/data/llama-7b-hf' \
-    --lora_weights '/home/vmagent/app/data/alpaca-lora-7b' \
-    --lora_weights_exp '/home/vmagent/app/data/zy_models/lora-alpaca'
+    --base_model '/home/vmagent/app/data/gpt-neo-125m' \
+    --lora_weights '/home/vmagent/app/data/zy_models/lora-temp'
--- scripts/train.sh
+++ scripts/train.sh
@@ -27,9 +27,37 @@ python my_finetune.py \
     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
     --output_dir '/home/vmagent/app/data/zy_models/lora-temp' \
     --batch_size 128 \
-    --num_epochs=1 \
+    --num_epochs=0.0025 \
     --cutoff_len=512 \
     --group_by_length \
     --lora_target_modules='[q_proj,v_proj]' \
     --lora_r=16 \
     --micro_batch_size=8
+
+
+# # gpt2, gpu, fp16
+# python my_finetune.py \
+#     --base_model '/home/vmagent/app/data/gpt2' \
+#     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
+#     --output_dir '/home/vmagent/app/data/zy_models/lora-temp' \
+#     --batch_size 128 \
+#     --num_epochs=1 \
+#     --cutoff_len=512 \
+#     --group_by_length \
+#     --lora_target_modules='[c_proj]' \
+#     --lora_r=16 \
+#     --micro_batch_size=8
+
+
+# # gpt-neo, gpu, fp16
+# python my_finetune.py \
+#     --base_model '/home/vmagent/app/data/gpt-neo-125m' \
+#     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
+#     --output_dir '/home/vmagent/app/data/zy_models/lora-temp' \
+#     --batch_size 128 \
+#     --num_epochs=0.03 \
+#     --cutoff_len=512 \
+#     --group_by_length \
+#     --lora_target_modules='[q_proj,v_proj]' \
+#     --lora_r=16 \
+#     --micro_batch_size=8
