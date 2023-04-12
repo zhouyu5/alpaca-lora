c50d154d13f5c3e318b24a5b2c17d38640f4baad
zhouyu5 <yu.zhou@intel.com>
2023-04-12 21:47:43 +0000

    exp

--- scripts/train.sh
+++ scripts/train.sh
@@ -10,34 +10,62 @@
 # python my_finetune.py \
 #     --base_model '/home/vmagent/app/data/llama-7b-hf' \
 #     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
-#     --output_dir '/home/vmagent/app/data/zy_models/lora-alpaca-1' \
+#     --output_dir '/home/vmagent/app/data/zy_models/lora-temp' \
 #     --batch_size 128 \
 #     --num_epochs=1 \
 #     --cutoff_len=512 \
 #     --group_by_length \
 #     --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' \
 #     --lora_r=16 \
-#     --micro_batch_size=8 \
-#     --fp16_train True
+#     --micro_batch_size=8 
 
 
-# gpt-j, gpu, fp16
+# llama-13b, gpu
 python my_finetune.py \
-    --base_model '/home/vmagent/app/data/gpt-j-6b' \
+    --base_model '/home/vmagent/app/data/llama-13b' \
     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
     --output_dir '/home/vmagent/app/data/zy_models/lora-temp' \
     --batch_size 128 \
-    --num_epochs=0.0025 \
+    --num_epochs=1 \
     --cutoff_len=512 \
     --group_by_length \
-    --lora_target_modules='[q_proj,v_proj]' \
+    --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' \
     --lora_r=16 \
-    --micro_batch_size=8
+    --micro_batch_size=8 \
+    --fp16_train=False
+
+
+# # gpt-j, gpu, fp16
+# python my_finetune.py \
+#     --base_model '/home/vmagent/app/data/gpt-j-6b' \
+#     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
+#     --output_dir '/home/vmagent/app/data/zy_models/lora-temp' \
+#     --batch_size 128 \
+#     --num_epochs=1 \
+#     --cutoff_len=512 \
+#     --group_by_length \
+#     --lora_target_modules='[q_proj,k_proj,v_proj,out_proj]' \
+#     --lora_r=16 \
+#     --micro_batch_size=8
 
 
 # # gpt2, gpu, fp16
 # python my_finetune.py \
 #     --base_model '/home/vmagent/app/data/gpt2' \
+#     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
+#     --output_dir '/home/vmagent/app/data/zy_models/lora-temp' \
+#     --batch_size 128 \
+#     --num_epochs=1 \
+#     --cutoff_len=512 \
+#     --group_by_length \
+#     --lora_target_modules='[c_proj]' \
+#     --lora_r=16 \
+#     --micro_batch_size=8
+
+
+# # gpt2-xl, gpu, fp16
+# python my_finetune.py \
+#     --base_model '/home/vmagent/app/data/gpt2-xl' \
 #     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
 #     --output_dir '/home/vmagent/app/data/zy_models/lora-temp' \
 #     --batch_size 128 \
@@ -55,9 +83,9 @@ python my_finetune.py \
 #     --data_path '/home/vmagent/app/data/alpaca-cleaned' \
 #     --output_dir '/home/vmagent/app/data/zy_models/lora-temp' \
 #     --batch_size 128 \
-#     --num_epochs=0.03 \
+#     --num_epochs=1 \
 #     --cutoff_len=512 \
 #     --group_by_length \
-#     --lora_target_modules='[q_proj,v_proj]' \
+#     --lora_target_modules='[q_proj,k_proj,v_proj,out_proj]' \
 #     --lora_r=16 \
 #     --micro_batch_size=8
