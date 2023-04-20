27041d363a84417b5475b7f87e2a2971fba38629
zhouyu5 <yu.zhou@intel.com>
2023-04-20 12:28:18 +0000

    demo

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,7 +1,12 @@
 # for int8 training, please set this flag
 # --load_8bit \    
 
+# python app.py \
+#     --share_gradio True \
+#     --base_model '/home/vmagent/app/data/gpt-neo-2.7B' \
+#     --lora_weights '/home/vmagent/app/data/zy_models/lora-gpt-neo-2.7B-alpaca'
+
 python app.py \
     --share_gradio True \
-    --base_model '/home/vmagent/app/data/gpt-neo-125m' \
-    --lora_weights '/home/vmagent/app/data/zy_models/lora-temp'
+    --base_model '/home/vmagent/app/data/llama-7b-hf' \
+    --lora_weights '/home/vmagent/app/data/alpaca-lora-7b'
